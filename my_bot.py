dd6a5116ce01faaeb54892f5ca72aa3eb9bb3186
Sphoneix <simone.pugliese21@gmail.com>
2018-06-18 23:30:55 +0200

    Add rl_stats

--- my_bot.py
+++ my_bot.py
@@ -13,6 +13,14 @@ parser.read('secret.ini')  # Configparser read file
 BOT_PREFIX = ("?", "!", ',')
 TOKEN = parser.get(section='secret', option='discord_token')
 ROCKET_LEAGUE_API_KEY = parser.get(section='secret', option='rocket_league_api_key')
+ROCKET_LEAGUE_TIERS = {0: 'Unranked', 1: 'Bronze I', 2: 'Bronze II', 3: 'Bronze III', 4: 'Silver I', 5: 'Silver II',
+                       6: 'Silver III',
+                       7: 'Gold I', 8: 'Gold II', 9: 'Gold III', 10: 'Platinum I', 11: 'Platinum II',
+                       12: 'Platinum III', 13: 'Diamond I',
+                       14: 'Diamond II', 15: 'Diamond III', 16: 'Champion I', 17: 'Champion II', 18: 'Champion III',
+                       19: 'Grand Champion'}
+ROCKET_LEAGUE_POINTS = (
+0, 192, 256, 316, 376, 443, 492, 555, 615, 692, 772, 852, 932, 1012, 1092, 1192, 1292, 1392, 1513, 1855)
 
 client = commands.Bot(command_prefix=BOT_PREFIX)
 
@@ -63,7 +71,31 @@ async def rocket(context):
         await client.say(f"Giocatore non trovato, {context.message.author.mention}")
 
 
-@client.command(name='teodoro',  # TEODORO WAKE UP
+@client.command(name='rl_rank',
+                description="Restituisce il ranking di RL durante l'attuale stagione",
+                brief='Per vedere ranking RL',
+                pass_context=True)
+async def rocket_rank(context):
+    custom_url = context.message.content[9:]
+    id_64 = steam.steamid.steam64_from_url('https://steamcommunity.com/id/' + custom_url)
+    razzo = rls.rocket.RocketLeague(ROCKET_LEAGUE_API_KEY)
+    try:
+        giocatore = razzo.players.player(id=id_64, platform=1).json()
+
+        await client.say(f"Rank di {giocatore['displayName']}:\n"
+                         f"__**1vs1**__ : {ROCKET_LEAGUE_TIERS[giocatore['rankedSeasons']['8']['10']['tier']]}, divisione {giocatore['rankedSeasons']['8']['10']['division']} :black_large_square: "
+                         f"**{giocatore['rankedSeasons']['8']['10']['rankPoints'] - min(ROCKET_LEAGUE_POINTS,key=lambda x:abs(x-8))}** punti per salire di rank :up: \n"
+                         f"__**2vs2**__ : {ROCKET_LEAGUE_TIERS[giocatore['rankedSeasons']['8']['11']['tier']]}, divisione {giocatore['rankedSeasons']['8']['11']['division']} :black_large_square: "
+                         f" **{giocatore['rankedSeasons']['8']['11']['rankPoints'] - min(ROCKET_LEAGUE_POINTS,key=lambda x:abs(x-8))}** punti per salire di rank :up: \n"
+                         f"__**3vs3**__ : {ROCKET_LEAGUE_TIERS[giocatore['rankedSeasons']['8']['13']['tier']]}, divisione {giocatore['rankedSeasons']['8']['13']['division']} :black_large_square: "
+                         f" **{giocatore['rankedSeasons']['8']['11']['rankPoints'] - min(ROCKET_LEAGUE_POINTS,key=lambda x:abs(x-8))}** punti per salire di rank :up: \n ")
+    except:
+        await client.say(f"Giocatore non trovato, {context.message.author.mention}")
+
+
+@client.command(name='teodoro',
+                description='Sveglia le persone sul server',
+                brief='TEODORO!',  # TEODORO WAKE UP
                 pass_context=True)
 @commands.cooldown(1, 120, commands.BucketType.server)
 async def teo(context):
