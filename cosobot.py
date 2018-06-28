7f2bee04f69d6a05b182029b306444bca5bef65d
Sphoneix <simone.pugliese21@gmail.com>
2018-06-28 23:40:03 +0200

    Add uptime

--- cosobot.py
+++ cosobot.py
@@ -1,18 +1,33 @@
-from configparser import ConfigParser
-from discord.ext import commands
-import discord
-import random
 import asyncio
+import random
 import time
+from configparser import ConfigParser
+
+import discord
+from discord.ext import commands
 
 parser = ConfigParser()  # Configparser start
 parser.read('secret.ini')  # Configparser read file
 
 BOT_PREFIX = ("?", "!", ',')
 TOKEN = parser.get(section='secret', option='discord_token')
-EXTENSION_LIST = ['cogs.rocket', 'cogs.error_handler','cogs.chat','cogs.tournaments']
+EXTENSION_LIST = ['cogs.rocket', 'cogs.error_handler', 'cogs.chat', 'cogs.tournaments']
 
 client = commands.Bot(command_prefix=BOT_PREFIX)
+
+
+def main():
+    for extension in EXTENSION_LIST:
+        try:
+            client.load_extension(extension)
+            print("Loaded {}".format(extension))
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+
+
+start_time = time.time()
+
 
 @client.event
 async def servers():
@@ -29,6 +44,7 @@ async def servers():
         print(" \n \n \n \n")
         await asyncio.sleep(300)
 
+
 @client.event
 async def game():
     await client.wait_until_ready()
@@ -44,17 +60,27 @@ async def game():
         await client.change_presence(activity=(discord.Game(random.choice(playing_list))))
         await asyncio.sleep(1000)
 
+
+@client.command(name='uptime')
+async def uptime(ctx):
+    tempo = time.time() - start_time
+    minute, second = divmod(tempo, 60)
+    hour, minute = divmod(minute, 60)
+    day, hour = divmod(hour, 24)
+    week, day = divmod(day, 7)
+    embed = discord.Embed(title='Bot attivo da:', description="{} settimane, {} giorni, "
+                                                              "{} ore, "
+                                                              "{} minuti, {} secondi".format(int(week), int(day),
+                                                                                             int(hour), int(minute),
+                                                                                             int(second)))
+    embed.set_footer(text='Last time started:', icon_url='https://png.icons8.com/color/1600/raspberry-pi.png')
+    await ctx.send(embed=embed)
+
+
 client.loop.create_task(servers())
 client.loop.create_task(game())
 
 if __name__ == '__main__':
-    for extension in EXTENSION_LIST:
-        try:
-            client.load_extension(extension)
-            print("Loaded {}".format(extension))
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
-
+    main()
 
 client.run(TOKEN)
