5a107837bd6b41cc2b13dc02fdbd9d9d82939c12
Sphoneix <simone.pugliese21@gmail.com>
2018-06-28 12:13:02 +0200

    Better code for stop command

--- cogs/chat.py
+++ cogs/chat.py
@@ -5,22 +5,23 @@ risposte = {
     'silvio': 'PALDINO!',
     'altro che': 'IL FUTTBOOL, FUTTBOOOOL!!',
     'obunga': 'AARGHHH!!',
-    'acribisignanoluzzi':'tu puzzi',
-    'pino':'il veneziano',
-    'peppe':'compra la ps4',
-    'gerardo':'...Paldino, conosciuto anche come re di Luzzi, '
-              'nonchè governatore supremo e protettore della Terra '
-              'tramite le forze armate, è una rispettabilissima persona degna di tale riconoscimenti.',
-    'sphoneix' : 'Esperto di fantaciclismo'
+    'acribisignanoluzzi': 'tu puzzi',
+    'pino': 'il veneziano',
+    'peppe': 'compra la ps4',
+    'gerardo': '...Paldino, conosciuto anche come re di Luzzi, '
+               'nonchè governatore supremo e protettore della Terra '
+               'tramite le forze armate, è una rispettabilissima persona degna di tale riconoscimenti.',
+    'sphoneix': 'Esperto di fantaciclismo'
 }
+
 
 class Chat():
     def __init__(self, client):
         self.client = client
 
     @commands.command(name='teodoro',
-                    description='Sveglia le persone sul server',
-                    brief='TEODORO!')
+                      description='Sveglia le persone sul server',
+                      brief='TEODORO!')
     @commands.cooldown(1, 120, commands.BucketType.guild)
     async def teodoro(self, ctx):
         teodoro = '<:teodoroemoji2:403218863967174667>'
@@ -41,15 +42,18 @@ class Chat():
                 return
 
     @commands.command(name='stop')
-    @commands.has_role('CosoAdmin')
     async def stop(self, ctx):
-        await ctx.send("Chiudendo il bot...")
-        await self.client.logout()
+        if await self.client.is_owner(ctx.author):
+            await ctx.send("Chiudendo il bot...")
+            await self.client.logout()
+        else:
+            await ctx.send('Ma chi ti credi di essere? Non sei mica il mio padrone!')
 
     @commands.command(name='github')
-    async def github(self,ctx):
+    async def github(self, ctx):
         await ctx.send(f"Tieni, {ctx.message.author.mention}. \n "
-                 f"https://github.com/sphoneix22/CosoBot")
+                       f"https://github.com/sphoneix22/CosoBot")
+
 
 def setup(client):
     client.add_cog(Chat(client))
