322d218fcd6e9f56457fc08a20f5911cfc70304a
Sphoneix <simone.pugliese21@gmail.com>
2018-07-05 14:35:26 +0200

    Better image search

--- cogs/google.py
+++ cogs/google.py
@@ -1,33 +1,43 @@
-from google_images_download import google_images_download
+import os
+
 import discord
 from discord.ext import commands
-import os
+from google_images_download import google_images_download
 
 response = google_images_download.googleimagesdownload()
 
 
 class GID():
     @classmethod
-    def downloader(cls,keyword):
-        opts = {'keywords':keyword,
-                'limit':1,
-                'output_directory':f"{os.path.dirname(os.getcwd())}/data/cache/images",
-                'no_directory':True}
+    def downloader(cls, keyword):
+        opts = {'keywords': keyword,
+                'limit': 1,
+                'output_directory': f"{os.getcwd()}/data/cache/images",
+                'no_directory': True,
+                'delay': 3,
+                'extract-metadata': True}
         path = response.download(opts)
         return path
 
+
 class Google():
-    def __init__(self,client):
+    def __init__(self, client):
         self.bot = client
 
     @commands.command('image')
-    @commands.cooldown(2,10,commands.BucketType.guild)
-    async def image(self,ctx):
+    @commands.cooldown(2, 10, commands.BucketType.guild)
+    async def image(self, ctx):
         async with ctx.typing():
             query = ctx.message.content[7:]
             path = GID.downloader(query)
             actual_path = path[query][0]
-            await ctx.send("**{}**".format(query),file=discord.File(actual_path))
+            await ctx.send("**{}**".format(query), file=discord.File(actual_path))
+
+    # todo text search
+
+    # todo wikipedia
+
+    # todo definizione
 
 
 def setup(client):
