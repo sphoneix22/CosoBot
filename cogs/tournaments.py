625e89f54cbc33290390cf914d20238fc0a406cd
Sphoneix <simone.pugliese21@gmail.com>
2018-07-07 19:55:21 +0200

    Removed partecipanti

--- cogs/tournaments.py
+++ cogs/tournaments.py
@@ -20,35 +20,11 @@ class Tournaments():
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(name='challonge')
-    async def chall(self, ctx):
-        challonge.set_credentials('bananaglassata', cf.get(section='secret', option='challonge_api'))
 
-    @chall.command(name='partecipanti')  # todo riconvertire questo comando in qualcosa di più utile e che funzioni
-    async def part(self, ctx):
-        async with ctx.typing():
-            torneo = ctx.message.content[24:]
-            tourn = challonge.tournaments.show(torneo)
-            part = challonge.participants.index(torneo)
-            embed = discord.Embed(title=f"Partecipanti del torneo '{tourn['name']}' di {tourn['game-name']}",
-                                  colour=discord.Colour(48942), url=tourn['full-challonge-url'],
-                                  timestamp=(tourn['started-at']))
-            embed.set_thumbnail(
-                url="https://challonge.com/assets/og-default-9d2c9e67cc219b24e19785afa8d289899116d96512f6096e67f04b854b2d174e.png")
-            embed.set_footer(text="Powered by Challonge API",
-                             icon_url="https://media.discordapp.net/attachments/258624439933992961/458288479864881152/COSOBOT.png")
-            for player in part:
-                if player['final-rank'] is None:
-                    embed.add_field(name=f"**{player['name']}**", value="Posizione: Il torneo non è ancora terminato!",
-                                    inline=True)
-                else:
-                    embed.add_field(name=f"**{player['name']}**", value=f"Posizione finale: {player['final-rank']}",
-                                    inline=True)
-        await ctx.channel.send(content=f"Ecco, {ctx.author.mention}", embed=embed)
 
-    @commands.command(name='stats_tornei')
+    @commands.command(name='tornei')
     @commands.cooldown(1, 60, commands.BucketType.user)
-    async def stats_tornei(self, ctx):
+    async def tornei(self, ctx):
         sh = client.open('Tornei Brawlhalla').sheet1
         embed = discord.Embed(title='Classifica tornei Brawlhalla',
                               url='https://docs.google.com/spreadsheets/d/1q9Hr8qrAUVpdq5OyV1SF4b7n5C2j0QGQg-JXXSJ1B8s'
@@ -62,7 +38,7 @@ class Tournaments():
                             inline=True)
         await ctx.send(embed=embed)
 
-    @commands.command(name='torneo_add')
+    @tornei.command(name='tornei_add')
     @commands.is_owner()
     async def add_tourn(self, ctx):
         if ctx.message.content[12:] not in partecipanti:
