2dd5ecfce8b6ad3c3b478f449da069d97b900145
Sphoneix <simone.pugliese21@gmail.com>
2018-07-16 12:05:17 +0200

    fix skip

--- cogs/music.py
+++ cogs/music.py
@@ -1,27 +1,30 @@
 import asyncio
+
 import discord
-from discord.ext import commands
-from youtube_dl import YoutubeDL
-from googleapiclient.discovery import build
 from async_timeout import timeout
+from discord.ext import commands
+from googleapiclient.discovery import build
+from youtube_dl import YoutubeDL
 
 API = 'youtube'
 API_V = 'v3'
 
 ytdl_opts = {
-    "format" : "bestaudio/best",
-    "outtmpl" : "./data/cache/music/%(extractor)s-%(id)s.%(ext)s",
-    "quiet" : True
+    "format": "bestaudio/best",
+    "outtmpl": "./data/cache/music/%(extractor)s-%(id)s.%(ext)s",
+    "quiet": True
 }
 
 ytdl = YoutubeDL(ytdl_opts)
+
 
 class YTDL:
     """
     Class with all YT search/download functions
     """
+
     @classmethod
-    async def YT_search(cls, query:str, api:str):
+    async def YT_search(cls, query: str, api: str):
         """
         Searches on youtube 5 videos and return a list of dictionaries with data.
         -----------------------------
@@ -29,7 +32,7 @@ class YTDL:
         :param api: str (api key)
         :return: list
         """
-        youtube = build(API,API_V,developerKey=api)
+        youtube = build(API, API_V, developerKey=api)
         search = youtube.search().list(
             part='snippet',
             q=query,
@@ -60,6 +63,7 @@ class MusicPlayer:
     The class implements a queue and loop to handle songs.
 
     """
+
     def __init__(self, ctx):
         self.bot = ctx.bot
         self._guild = ctx.guild
@@ -81,7 +85,7 @@ class MusicPlayer:
             self.next.clear()
 
             try:
-                #Wait for song. If timeout closes connection
+                # Wait for song. If timeout closes connection
                 async with timeout(120):
                     source = await self.queue.get()
             except asyncio.TimeoutError:
@@ -91,26 +95,26 @@ class MusicPlayer:
 
             self.current = source
 
-            self._guild.voice_client.play(source[0], after= lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
+            self._guild.voice_client.play(source[0], after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
             self.np = await self._channel.send(f"Ora suono: **{source[1]['title']}**")
 
             await self.next.wait()
 
-            source[0].cleanup() #Cleanup FFmpeg process
-            self.current = None #Not playing at the moment
+            source[0].cleanup()  # Cleanup FFmpeg process
+            self.current = None  # Not playing at the moment
 
             try:
-                await self.np.delete() #No longer playing
+                await self.np.delete()  # No longer playing
             except discord.HTTPException:
                 pass
 
-    def destroy(self,guild):
+    def destroy(self, guild):
         """Disconnect"""
         return self.bot.loop.create_task(self._cog.cleanup(guild))
 
 
 class Music:
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
         self.players = {}
         self.skips = {}
@@ -118,7 +122,6 @@ class Music:
     async def cleanup(self, guild):
         await guild.voice_client.disconnect()
         del self.players[guild.id]
-
 
     def get_player(self, ctx):
         """Retrieve guild player or generate one"""
@@ -138,7 +141,7 @@ class Music:
         else:
             return await ctx.voice_client.move_to(ctx.message.author.voice.channel)
 
-    async def get_msg(self, result :list):
+    async def get_msg(self, result: list):
         """
         Creates a choose msg from a list of videos.
 
@@ -152,44 +155,44 @@ class Music:
         return msg
 
     async def skip_counter(self, users, ctx):
-        if len(users)-1 % 2 == 0:
-            max_limit = len(users)-1 / 2
+        if len(users) - 1 % 2 == 0:
+            max_limit = len(users) - 1 / 2
         else:
             max_limit = len(users) // 2
 
         try:
-            if max_limit == self.skips[ctx.guild.id]:
+            if max_limit == len(self.skips[ctx.guild.id]):
                 await ctx.send("Ok, skippo...")
                 ctx.voice_client.stop()
             else:
-                await ctx.send(f"Mancano ancora {max_limit-self.skips[ctx.guild.id]+1} voti.")
-                self.skips[ctx.guild.id] += 1
+                await ctx.send(f"Mancano ancora {max_limit-len(self.skips[ctx.guild.id])+1} voti.")
+                self.skips[ctx.guild.id].append(ctx.message.author.id)
 
         except KeyError:
             if max_limit == 1:
                 await ctx.send(f"Ok, skippo...")
                 return ctx.voice_client.stop()
             await ctx.send(f"Mancano ancora {max_limit-1} voti.")
-            self.skips[ctx.guild.id] = 1
-
+            self.skips[ctx.guild.id].append(ctx.message.author.id)
 
     @commands.command(name='play')
-    async def play(self,ctx):
-        result = await YTDL.YT_search(ctx.message.content[6:],self.bot.secrets['google_api_key'])
+    async def play(self, ctx):
+        result = await YTDL.YT_search(ctx.message.content[6:], self.bot.secrets['google_api_key'])
         choose_msg = await self.get_msg(result)
         await ctx.send(choose_msg)
-        #Now we have sent the message to choose the video from, let's wait for an asnwer
+
+        # Now we have sent the message to choose the video from, let's wait for an asnwer
 
         def check(m):
             """
             Checks if the message is sent by the user and if the number is valid
             """
-            return m.author == ctx.message.author and m.channel == ctx.message.channel and int(m.content) in range(1, len(result)+1)
+            return m.author == ctx.message.author and m.channel == ctx.message.channel and int(m.content) in range(1,len(result) + 1)
 
         try:
-            msg = await self.bot.wait_for("message",timeout=60,check=check)
+            msg = await self.bot.wait_for("message", timeout=60, check=check)
             await ctx.send("Ok, canzone scelta.")
-            id = result[int(msg.content)-1]['id']['videoId']  #gets video id by the index of the list
+            id = result[int(msg.content) - 1]['id']['videoId']  # gets video id by the index of the list
             vc = await self.join(ctx)
             song = YTDL.downloader(id)
             player = self.get_player(ctx)
@@ -197,16 +200,26 @@ class Music:
         except asyncio.TimeoutError:
             await ctx.send("Ok, non la suono più")
 
+    @commands.command(name='nowplaying', aliases=['np', 'song'])
+    async def nowplaying_(self, ctx):
+        if ctx.guild.id in self.players.keys():
+            np = self.players[ctx.guild.id].np
+            return await ctx.send(np.content)
+        return
+
     @commands.command(name='skip')
     async def skip_(self, ctx):
         vc = ctx.voice_client
 
         if not vc or not vc.is_connected():
             return
+        try:
+            if ctx.message.author.id in self.skips[ctx.guild.id]:
+                return
+        except KeyError:
+            pass
 
-        await self.skip_counter(vc.channel.members,ctx)
-
-
+        await self.skip_counter(vc.channel.members, ctx)
 
     @commands.command(name='pause')
     async def pause_(self, ctx):
@@ -239,7 +252,7 @@ class Music:
         await ctx.send(f"Ho ricominciato a suonare, {ctx.message.author.mention}")
 
     @commands.command(name='stop')
-    async def stop_(self,ctx):
+    async def stop_(self, ctx):
         """
         Stops and deletes the player.
         """
@@ -249,6 +262,7 @@ class Music:
             return await ctx.send(f"Ma che vuoi? {ctx.message.author.mention}")
 
         await self.cleanup(ctx.guild)
+
 
 def setup(bot):
     bot.add_cog(Music(bot=bot))
